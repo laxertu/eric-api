02a3dd7110bb129aeb7590ed91766d6381409cf3
luca@igor <luca@igor>
2024-09-15 01:16:10 +0200

    add http client close support

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import logging
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from eric.model import SSEChannel, MessageQueueListener, Message
 from sse_starlette.sse import EventSourceResponse
 
 server_channel = SSEChannel()
 
 class ApiMessageListener(MessageQueueListener):
+
+    def __init__(self, request: Request):
+
+        super().__init__()
+        self.__request = request
+
+    async def is_running(self) -> bool:
+        if await self.__request.is_disconnected():
+            return False
+        else:
+            return await super().is_running()
 
     def on_message(self, msg: Message) -> None:
         logging.info("Message received")
@@ -28,6 +39,6 @@ async def send(listener_id: str, msg: Message):
     server_channel.dispatch(listener=server_channel.get_listener(listener_id), msg=msg)
 
 @app.get("/stream/{listener_id}")
-async def stream(listener_id: str):
+async def stream(request: Request, listener_id: str):
     listener = server_channel.get_listener(listener_id)
     return EventSourceResponse(await server_channel.message_stream(listener))
