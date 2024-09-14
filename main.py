0610dbf729936156e27605ed5bdffe7b29a2bb29
luca@igor <luca@igor>
2024-09-14 18:10:39 +0200

    minor changes

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-from fastapi import FastAPI, Request
+import logging
+
+from fastapi import FastAPI
 from eric.model import SSEChannel, MessageQueueListener, Message
 from sse_starlette.sse import EventSourceResponse
 
-
 server_channel = SSEChannel()
 
-class BroadCastListener(MessageQueueListener):
+class ApiMessageListener(MessageQueueListener):
 
     def on_message(self, msg: Message) -> None:
-        server_channel.broadcast(msg)
+        logging.info("Message received")
 
 app = FastAPI()
 
 @app.post("/subscribe")
 async def subscribe():
-    l = server_channel.add_listener(BroadCastListener)
+    l = server_channel.add_listener(ApiMessageListener)
     return {"listener_id": l.id}
 
 @app.post("/broadcast")
@@ -22,12 +23,11 @@ async def broadcast(msg: Message):
     server_channel.broadcast(msg)
     return None
 
-@app.post("/send_to")
-async def send_to(listener_id: str, msg: Message):
+@app.post("/dispatch")
+async def send(listener_id: str, msg: Message):
     server_channel.dispatch(listener=server_channel.get_listener(listener_id), msg=msg)
 
-@app.get("/game/stream/{session_id}/{listener_id}")
-async def message_stream(listener_id: str):
+@app.get("/stream/{listener_id}")
+async def stream(listener_id: str):
     listener = server_channel.get_listener(listener_id)
     return EventSourceResponse(await server_channel.message_stream(listener))
-
