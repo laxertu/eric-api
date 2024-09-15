c7198a64831f9facbdf2eb43391369a9a684d1e0
luca@igor <luca@igor>
2024-09-15 09:48:19 +0200

    add multi channel support

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
-import logging
-
 from fastapi import FastAPI, Request
-from eric.model import SSEChannel, MessageQueueListener, Message
+from eric.model import MessageQueueListener, Message
+from eric.servers import ChannelContainer
 from sse_starlette.sse import EventSourceResponse
 
-server_channel = SSEChannel()
-
-class ApiMessageListener(MessageQueueListener):
-    ...
-
+channel_container = ChannelContainer()
 
 app = FastAPI()
 
 @app.post("/subscribe")
-async def subscribe():
-    l = ApiMessageListener()
-    l = server_channel.add_listener(ApiMessageListener)
+async def subscribe(channel_id: str):
+    channel_container.get(channel_id).add_listener(MessageQueueListener)
+    l = channel_container.get(channel_id).add_listener(MessageQueueListener)
     return {"listener_id": l.id}
 
 @app.post("/broadcast")
-async def broadcast(msg: Message):
-    server_channel.broadcast(msg)
+async def broadcast(channel_id: str, msg: Message):
+    channel_container.get(channel_id).broadcast(msg)
     return None
 
 @app.post("/dispatch")
-async def send(listener_id: str, msg: Message):
-    server_channel.dispatch(listener=server_channel.get_listener(listener_id), msg=msg)
+async def send(channel_id: str, listener_id: str, msg: Message):
+    channel = channel_container.get(channel_id)
+    channel.dispatch(listener=channel.get_listener(listener_id), msg=msg)
 
-@app.get("/stream/{listener_id}")
-async def stream(request: Request, listener_id: str):
-    listener = server_channel.get_listener(listener_id)
+@app.get("/stream/{channel_id}/{listener_id}")
+async def stream(request: Request, channel_id: str, listener_id: str):
+    channel = channel_container.get(channel_id)
+    listener = channel.get_listener(listener_id)
     if request.is_disconnected():
-        server_channel.dispatch(listener, Message(type="connection_closed"))
+        channel.dispatch(listener, Message(type="connection_closed"))
         await listener.stop()
-    return EventSourceResponse(await server_channel.message_stream(listener))
+    return EventSourceResponse(await channel.message_stream(listener))
