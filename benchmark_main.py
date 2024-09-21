913d8a89becc7e2e1890872bf1396e534066791b
luca@igor <luca@igor>
2024-09-21 20:53:53 +0200

    added benchmark code

--- benchmark_main.py
+++ benchmark_main.py
@@ -4,25 +4,28 @@ from logging import getLogger
 
 from fastapi import FastAPI, Request
 from eric.entities import Message, DataProcessingChannel, ThreadPoolListener
+from pydantic.dataclasses import dataclass
 from sse_starlette.sse import EventSourceResponse
 
 logger = getLogger(__name__)
-
-PROCESS_TIME = 0.02
-BLOCKING_OPERATION_TIME = 0.5
-FIXTURE_SIZE = 1000
 MAX_WORKERS = 6
 
+@dataclass
+class BenchmarkParams:
+    item_process_time: float
+    blocking_operation_time: float
+    fixture_size: int
 
-def create_fixture() -> list[dict]:
-    return [{f'x': x} for x in range(1, FIXTURE_SIZE)]
 
-async def do_blocking_request(response: list[dict]):
-    await asyncio.sleep(BLOCKING_OPERATION_TIME)
+def create_fixture(size: int) -> list[dict]:
+    return [{f'x': x} for x in range(1, size)]
+
+async def do_blocking_request(time_to_wait: float, response: list[dict]):
+    await asyncio.sleep(time_to_wait)
     return response
 
-def process_item(item: dict) -> dict:
-    time.sleep(PROCESS_TIME)
+def process_item(time_to_wait: float, item: dict) -> dict:
+    time.sleep(time_to_wait)
     return item
 
 
@@ -32,20 +35,19 @@ channel.register_listener(listener_sse)
 
 app = FastAPI()
 
-@app.get("/start_get")
-async def create():
-    response = await do_blocking_request(create_fixture())
-    return [process_item(x) for x in response]
+@app.post("/start_get")
+async def get_data(params: BenchmarkParams):
+    response = await do_blocking_request(params.blocking_operation_time, create_fixture(params.fixture_size))
+    return [process_item(params.item_process_time, x) for x in response]
 
 
-@app.get("/start_sse")
-async def stream(request: Request):
-    response = await do_blocking_request(create_fixture())
+@app.post("/start_sse")
+async def stream_data(request: Request, params: BenchmarkParams):
+    response = await do_blocking_request(params.blocking_operation_time, create_fixture(params.fixture_size))
     for m in response:
         channel.dispatch(listener_sse.id, Message(type='test', payload=m))
     channel.dispatch(listener_sse.id, Message(type='_eric_channel_closed'))
     await listener_sse.start()
-    logger.info(f"wget -q -S -O - 127.0.0.1:8000/start_sse 2>&1")
     if await request.is_disconnected():
         await listener_sse.stop()
 
--- bm.py
+++ bm.py
@@ -1,12 +1,24 @@
+import sys
+from concurrent.futures import ThreadPoolExecutor
 import requests, time
 
-st = time.time()
-x = requests.get('http://127.0.0.1:8000/start_get')
-print(time.time() - st)
+benchmark_params = {
+  "item_process_time": float(sys.argv[1]),
+  "blocking_operation_time": float(sys.argv[2]),
+  "fixture_size": int(sys.argv[3])
+}
 
+print("Launching with", benchmark_params)
 
-st = time.time()
-x = requests.get('http://127.0.0.1:8000/start_sse')
-print(time.time() - st)
+def launch(label: str, callabe, **kwargs):
+    st = time.time()
+    callabe(**kwargs)
+    print(f"Time {label}", time.time() - st)
+
+with ThreadPoolExecutor(max_workers=2) as e:
+
+    launch(callabe=requests.post, label='naive', url='http://127.0.0.1:8000/start_get', json=benchmark_params)
+    launch(callabe=requests.post, label='threaded', url='http://127.0.0.1:8000/start_sse', json=benchmark_params)
+
 
 
