cd0c0d2860d07e4594827141b487db1b0b4f34a6
luca@igor <luca@igor>
2024-09-21 18:38:10 +0200

    added benchmark code

--- benchmark_main.py
+++ benchmark_main.py
@@ -1,42 +1,18 @@
 import asyncio
 import time
 from logging import getLogger
-from typing import Any
+
 from fastapi import FastAPI, Request
-from eric.entities import Message, MessageQueueListener, AbstractChannel
+from eric.entities import Message, DataProcessingChannel, ThreadPoolListener
 from sse_starlette.sse import EventSourceResponse
-from concurrent.futures import ThreadPoolExecutor
 
 logger = getLogger(__name__)
 
 PROCESS_TIME = 0.02
 BLOCKING_OPERATION_TIME = 0.5
 FIXTURE_SIZE = 1000
+MAX_WORKERS = 6
 
-class BenchMarkChannel(AbstractChannel):
-
-    def adapt(self, msg: Message) -> Any:
-        return None
-
-    def notify_end(self):
-        self.broadcast(Message(type='end'))
-
-
-class BenchMarkListener(MessageQueueListener):
-    def __init__(self):
-        super().__init__()
-        self.executor = ThreadPoolExecutor(max_workers=6)
-
-    def on_message(self, msg: Message) -> None:
-        if msg.type == 'end':
-            self.stop_sync()
-        else:
-            self.executor.submit(process_item, msg.payload)
-
-
-channel = BenchMarkChannel()
-listener_sse = BenchMarkListener()
-channel.register_listener(listener_sse)
 
 def create_fixture() -> list[dict]:
     return [{f'x': x} for x in range(1, FIXTURE_SIZE)]
@@ -52,6 +28,11 @@ def process_item(item: dict) -> dict:
     time.sleep(PROCESS_TIME)
     return item
 
+
+channel = DataProcessingChannel()
+listener_sse = ThreadPoolListener(callback=process_item, max_workers=MAX_WORKERS)
+channel.register_listener(listener_sse)
+
 app = FastAPI()
 
 @app.get("/start_get")
@@ -65,7 +46,7 @@ async def stream(request: Request):
     response = await do_blocking_request(create_fixture())
     for m in response:
         channel.dispatch(listener_sse.id, Message(type='test', payload=m))
-    channel.dispatch(listener_sse.id, Message(type='end'))
+    channel.dispatch(listener_sse.id, Message(type='_eric_channel_closed'))
     await listener_sse.start()
     # logger.info(f"wget -q -S -O - 127.0.0.1:8000/stream/{channel_id}/{listener_id} 2>&1")
     if await request.is_disconnected():
