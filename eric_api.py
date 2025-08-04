6c4943e64197edfae148305d50f5083ae6c672cd
luca <laxertu@gmail.com>
2025-08-04 11:05:02 +0200

    1.5.0 autorefresh channels in case repo has been written by other client

--- eric_api.py
+++ eric_api.py
@@ -1,18 +1,22 @@
 import logging
 import traceback
+from os import getenv
+from typing import Iterable
 
+from dotenv import load_dotenv
 from logging import getLogger
+
+from pydantic import BaseModel
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from sse_starlette.sse import EventSourceResponse
+
 from eric_sse.entities import Message
 from eric_sse.prefabs import SSEChannel
 from eric_sse.servers import ChannelContainer
 from eric_sse.exception import InvalidChannelException, InvalidListenerException
-from pydantic import BaseModel
-from sse_starlette.sse import EventSourceResponse
-from os import getenv
-from dotenv import load_dotenv
 
+from eric_redis_queues import RedisConnectionsRepository, RedisSSEChannelRepository
 
 load_dotenv('.eric-api.env')
 logger = getLogger('uvicorn.error')
@@ -23,7 +27,7 @@ queues_factory = None
 channel_repository = None
 if getenv("QUEUES_FACTORY") == "redis":
     logger.info('Setting up redis queues')
-    from eric_redis_queues import RedisConnectionsRepository, RedisSSEChannelRepository
+
     queues_factory = RedisConnectionsRepository(
         host=getenv("REDIS_HOST", "127.0.0.1"),
         port=int(getenv("REDIS_PORT", "6379")),
@@ -39,6 +43,27 @@ if getenv("QUEUES_FACTORY") == "redis":
     for channel in channel_repository.load():
         channel.load_persisted_data()
         channel_container.register(channel)
+
+
+def refresh_channels():
+    registered_ids = set(channel_container.get_all_ids())
+    for persisted_channel in channel_repository.load():
+        if persisted_channel.id not in registered_ids:
+            channel_container.register(persisted_channel)
+
+
+def get_channel(channel_id: str):
+
+    try:
+        return channel_container.get(channel_id)
+    except InvalidChannelException:
+        logger.debug(f'No channel found with id {channel_id}. Reloading all channels')
+        print(f'No channel found with id {channel_id}. Reloading all channels')
+        if channel_repository is not None:
+            refresh_channels()
+            return channel_container.get(channel_id)
+
+
 
 class MessageDto(BaseModel):
     type: str
@@ -70,6 +95,7 @@ async def exception_handler(request: Request, exc: Exception):
 
 @app.get("/channels")
 async def get_channels(request: Request):
+    refresh_channels()
     return [i for i in channel_container.get_all_ids()]
 
 @app.put("/create")
@@ -84,17 +110,17 @@ async def create():
 
 @app.post("/subscribe")
 async def subscribe(channel_id: str):
-    l = channel_container.get(channel_id).add_listener()
+    l = get_channel(channel_id).add_listener()
     return {"listener_id": l.id}
 
 @app.post("/broadcast")
 async def broadcast(channel_id: str, msg: MessageDto):
-    channel_container.get(channel_id).broadcast(msg.to_message())
+    get_channel(channel_id).broadcast(msg.to_message())
     return None
 
 @app.post("/dispatch")
 async def send(channel_id: str, listener_id: str, msg: MessageDto):
-    channel_container.get(channel_id).dispatch(listener_id, msg.to_message())
+    get_channel(channel_id).dispatch(listener_id, msg.to_message())
 
 @app.get("/stream/{channel_id}/{listener_id}")
 async def stream(request: Request, channel_id: str, listener_id: str):
@@ -105,15 +131,15 @@ async def stream(request: Request, channel_id: str, listener_id: str):
 
     ***wget -q -S -O - 127.0.0.1:8000/stream/{channel_id}/{listener_id} 2>&1***
     """
-    listener = channel_container.get(channel_id).get_listener(listener_id)
+    listener = get_channel(channel_id).get_listener(listener_id)
     listener.start()
     if await request.is_disconnected():
         listener.stop()
-    return EventSourceResponse(channel_container.get(channel_id).message_stream(listener))
+    return EventSourceResponse(get_channel(channel_id).message_stream(listener))
 
 @app.delete("/listener/{channel_id}/{listener_id}")
 async def delete_listener(channel_id: str, listener_id: str):
-    channel_container.get(channel_id).remove_listener(listener_id)
+    get_channel(channel_id).remove_listener(listener_id)
     if channel_repository is not None:
         channel_repository.delete_listener(channel_id, listener_id)
 
@@ -130,6 +156,7 @@ async def delete_channel(channel_id: str):
 
 @app.get("/")
 async def root():
+    refresh_channels()
     result = {}
     for channel_id in channel_container.get_all_ids():
         result[channel_id] = [listener_id for listener_id in channel_container.get(channel_id).get_listeners_ids()]
